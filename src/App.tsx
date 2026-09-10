3760d06aa1910d00d350bc8ba5e4b596d00296cd
magnus <magnus@udel.edu>
2026-09-10 14:49:17 -0400

    removed other members of FFL, added some videos and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
 import { NameButton } from './NameButton';
+import {MagnusPortfolio} from './MagnusPortfolio';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p className='top-text'>Meet FFL's team of highly esteemed engineers:</p>
+        <p className='top-text'>Magnus Culley Project Showcase</p>
+        <p>Links: <a style={{ color: 'lightblue' }} href="https://www.linkedin.com/in/magnus-culley-081b33297/" target="_blank" rel="noopener noreferrer">LinkedIn</a> | <a style={{ color: 'lightblue' }} href="https://github.com/magnusculley" target="_blank" rel="noopener noreferrer">GitHub</a></p>
       </header>
-      <div>
-        <NameButton></NameButton>
-      </div>
+      <MagnusPortfolio/>
     </div>
   );
 }
--- src/MagnusPortfolio.tsx
+++ src/MagnusPortfolio.tsx
@@ -6,11 +6,19 @@ export function MagnusPortfolio():React.JSX.Element{
         <div>
             <div className='grid'>
                 <div className="video-container" id="column1">
+                    <iframe className="video"  src="https://www.youtube.com/embed/KpCysp8j-Pg?si=ptLp6mWl6ZV3jSUC" allowFullScreen title="TumbleTracker"></iframe>
                     <iframe className="video"  src="https://youtube.com/embed/VaP5ZQPbNB0" allowFullScreen title="HONR291"></iframe>
                     <iframe className="video"  src="https://youtube.com/embed/vlJ8E4v2IQM" allowFullScreen title="Orwell"></iframe>
+                    <iframe className="video"  src="https://www.youtube.com/embed/RVgxDD6JTLc?si=gni6S9Ni8-w5Hy6X" allowFullScreen title="Ball Game"></iframe>
                     <iframe className="video"  src="https://youtube.com/embed/YhBT5LQWPfI" allowFullScreen title="Interview"></iframe>
                 </div>
                 <div>
+                <h1 className='title'>
+                    Hackathon Project:
+                </h1>
+                <p className='desc'>
+                    This was the project I made with a team for HenHacks 2026. Our goal was to challenge ourselves and make something usable in 24 hours.
+                </p>    
                 <h1 className='title'>
                     HONR291 Final Project:
                 </h1>
@@ -23,6 +31,12 @@ export function MagnusPortfolio():React.JSX.Element{
                 <p className='desc'>
                     This was my project for HenHacks 2025. Ben, Andrew, and I created a discord bot that read messages and used AI to compare the content against the ruleset of the server to determine if it was appropriate.
                 </p >
+                <h1 className='title'>
+                    Freshman Game:
+                </h1>
+                <p className='desc'>
+                    This is a game I made in freshman year. It is made in Python with Drafter.
+                </p>
                 <h1 className='title'>
                     Random Video:
                 </h1>
