0614b1fee9325bc283164c92bb9c2deb87fdaf72
magnus <magnus@udel.edu>
2026-09-10 15:02:54 -0400

    fixed typo

--- src/MagnusPortfolio.tsx
+++ src/MagnusPortfolio.tsx
@@ -23,7 +23,7 @@ export function MagnusPortfolio():React.JSX.Element{
                     HONR291 Final Project:
                 </h1>
                 <p className='desc'>
-                    For this class I chose to make my final project a game. It combines literally analysis with puzzles in the style of a point and click. I solely developed it in Godot Game Engine.
+                    For this class I chose to make my final project a game. It combines literary analysis with puzzles in the style of a point and click. I solely developed it in Godot Game Engine.
                 </p>
                 <h1 className='title'>
                     Orwell Discord Moderation:
